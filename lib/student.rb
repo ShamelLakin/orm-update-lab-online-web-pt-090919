bfb37a094b06a585a9ec4bf6abcf7e3f9fcff678
Shamel Lakin <christlife4u@gmail.com>
2019-11-14 14:17:24 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -13,7 +13,10 @@ class Student
   
   def self.create_table 
     sql = <<- SQL 
-    
+    CREATE TABLE IF NOT EXISTS students(
+      
+      )
+    SQL
   end 
 
   # Remember, you can access your database connection anywhere in this class
