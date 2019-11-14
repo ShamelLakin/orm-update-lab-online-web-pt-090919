9f18a1776dbb6a5311ccbb8eb350cbe23f16f172
Shamel Lakin <christlife4u@gmail.com>
2019-11-14 14:42:49 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -50,7 +50,6 @@ class Student
     student = Student.new(name, grade)
     student.save
     student
-    # DB[:conn].execute(sql)
   end 
 
   # Remember, you can access your database connection anywhere in this class
