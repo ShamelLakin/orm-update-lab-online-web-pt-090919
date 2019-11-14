1c63eceb0ff54db8d178f6ed53a1abac54acfda6
Shamel Lakin <christlife4u@gmail.com>
2019-11-14 14:29:25 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -29,8 +29,9 @@ class Student
   
   def save 
     if self.id
-    self.update
-    else
+      self.update
+      else
+    end 
     sql = <<-SQL
       INSERT INTO students (name, grade) 
       VALUES (?, ?)
