6c78f20b4742cbf01f0b3636b3290842449c46fa
Shamel Lakin <christlife4u@gmail.com>
2019-11-14 14:23:49 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -22,8 +22,9 @@ class Student
     DB[:conn].execute(sql)
   end 
   
-  def self.droptable
-    
+ def self.drop_table
+    sql = "DROP TABLE IF EXISTS students"
+    DB[:conn].execute(sql) 
   end 
 
   # Remember, you can access your database connection anywhere in this class
